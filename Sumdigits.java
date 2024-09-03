01596a31be2291c5c9049001d46abc2075a2e11c
Thirupathamma2001Y <yalagala2001@gmail.com>
2024-09-03 20:14:08 +0530

    Add files via upload

---
+++ Sumdigits.java
@@ -0,0 +1,15 @@
+class Sum
+{
+	public static void main(String[] args) 
+	{
+		int num =2345;
+		int sum;
+		sum=0;
+		while(n>0)
+		{
+			sum = sum+num%10;
+			num =num/10;
+		}
+		System.out.println("print the sum of digits =" sum);
+	}
+}
---
+++ productDetailes.java
@@ -0,0 +1,14 @@
+class ProductDetailes 
+{
+	public static void main(String[] args) 
+	{
+
+       int productId = 222;
+	   String productName = "Book";
+	   float  price        = 234.7f;
+
+		System.out.println("print the productid "+ productId);
+		System.out.println("print the productname "+ productName);
+		System.out.println("print the productprice "+ price);
+	}
+}
